62850315b53be6844d8da816d1e3a173915fab62
HelmerNylen <helmer@nylen.info>
2021-02-04 22:35:20 +0100

    Man kan promenera runt

--- js/entity.js
+++ js/entity.js
@@ -12,6 +12,9 @@ const Direction = {
 	"North": 3
 };
 
+// Milliseconds to move one tile
+const ENTITY_PACE = 500;
+
 class Movement {
 	constructor(type, data = null) {
 		this.type = type;
@@ -31,14 +34,35 @@ class Movement {
 				break;
 		}
 	}
+
+	static interpolateX(gridX, direction, progress) {
+		if (direction === Direction.East)
+			return gridX - 1 + progress;
+		else if (direction === Direction.West)
+			return gridX + 1 - progress;
+		else
+			return gridX;
+	}
+
+	static interpolateY(gridY, direction, progress) {
+		if (direction === Direction.North)
+			return gridY + 1 - progress;
+		else if (direction === Direction.South)
+			return gridY - 1 + progress;
+		else
+			return gridY;
+	}
 }
 
 class AnimationSet {
 	constructor(tilesets, json) {
 		this.frameHeight = json.frameHeight;
+		this.length = {};
 		for (const dir of Object.keys(Direction)) {
-			this[dir.toLowerCase()] = Level.makePatch(tilesets, json[dir.toLowerCase()]);
-			this[Direction[dir]] = this[dir.toLowerCase()];
+			if (json[dir.toLowerCase()]) {
+				this[Direction[dir]] = Level.makePatch(tilesets, json[dir.toLowerCase()]);
+				this.length[Direction[dir]] = Math.ceil(this[Direction[dir]].height / this.frameHeight);
+			}
 		}
 	}
 
@@ -57,20 +81,86 @@ class Entity {
 	constructor (x, y, direction, animationSet, movement) {
 		this.x = x;
 		this.y = y;
+		this.gridX = x;
+		this.gridY = y;
 		this.direction = direction;
 		if (this.direction === undefined)
 			this.direction = Direction.South;
 		this.animationSet = animationSet;
 		this.movement = movement;
+		this.movementProgress = null;
+		this.subimage = 0;
+
+		this._currentSprite = null;
 	}
 
 	get currentSprite() {
-		// TODO: faktiskt animera, cachea animering etc
-		return this.animationSet.getFrame(this.direction, 0);
+		if (this._currentSprite === null)
+			this._currentSprite = this.animationSet.getFrame(this.direction, this.subimage);
+			
+		return this._currentSprite;
 	}
 
 	static createEntity(tilesets, json) {
 		const animationSet = new AnimationSet(tilesets, json.animationSet);
-		return new Entity(json.x, json.y, json["direction"], animationSet, new Movement(json.movement.type, json.movement["data"] || null))
+		return new Entity(
+			json.x, json.y,
+			json["direction"],
+			animationSet,
+			new Movement(MovementType[json.movement.type], json.movement["data"] || null)
+		);
+	}
+
+	update(delta, userinput) {
+		// If responding to player input and input is received,
+		// set direction and start walking
+		if (this.movement.type === MovementType.Player && userinput !== null) {
+			if (this.movementProgress === null) {
+				let inputAccepted = true;
+				switch (userinput) {
+					case Direction.North:
+						this.gridY--;
+						break;
+					case Direction.South:
+						this.gridY++;
+						break;
+					case Direction.East:
+						this.gridX++;
+						break;
+					case Direction.West:
+						this.gridX--;
+						break;
+					default:
+						inputAccepted = false;
+						break;
+				}
+				if (inputAccepted) {
+					this.direction = userinput;
+					this._currentSprite = null;
+					this.movementProgress = 0;
+				}
+			}
+		}
+
+		// Stop moving if destination reached
+		if (this.movementProgress === 1) {
+			this.movementProgress = null;
+			this.subimage = 0;
+			this._currentSprite = null;
+		} else if (this.movementProgress !== null) {
+			// Update distance moved
+			this.movementProgress = Math.min(1, this.movementProgress + delta / ENTITY_PACE);
+
+			// Compute new animation frame
+			const newSubimage = Math.min(this.animationSet.length[this.direction] - 1, Math.floor(this.movementProgress * this.animationSet.length[this.direction]));
+			if (newSubimage !== this.subimage) {
+				this.subimage = newSubimage;
+				this._currentSprite = null;
+			}
+
+			// Update position
+			this.x = Movement.interpolateX(this.gridX, this.direction, this.movementProgress);
+			this.y = Movement.interpolateY(this.gridY, this.direction, this.movementProgress);
+		}
 	}
 }
--- js/exploration.js
+++ js/exploration.js
@@ -6,6 +6,34 @@ class ExplorationController {
 		this.entities = null;
 		this.currentLevel = null;
 		this.loadingMessage = null;
+
+		this.timestampLast = null;
+		window.requestAnimationFrame(this.update.bind(this));
+		
+		this.userinput = null;
+		document.body.addEventListener('keydown', e => {
+			switch (e.code) {
+				case 'KeyW':
+				case 'ArrowUp':
+					this.userinput = Direction.North;
+					break;
+				case 'KeyS':
+				case 'ArrowDown':
+					this.userinput = Direction.South;
+					break;
+				case 'KeyA':
+				case 'ArrowLeft':
+					this.userinput = Direction.West;
+					break;
+				case 'KeyD':
+				case 'ArrowRight':
+					this.userinput = Direction.East;
+					break;
+				default:
+					return true;
+			}
+			e.preventDefault();
+		});
 	}
 
 	async loadLevel(level) {
@@ -23,15 +51,39 @@ class ExplorationController {
 		this.terrain = await Level.createTerrain(json);
 		this.cellsize = json.terrain.cellsize;
 		this.entities = await Level.createEntities(json);
-		const ctx = this.canvas.getContext('2d');
+	}
 
-		ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-		ctx.drawImage(this.terrain, 0, 0);
-		this.entities.forEach(entity => ctx.drawImage(
-			entity.currentSprite,
-			this.cellsize * entity.x,
-			this.cellsize * entity.y
-		));
+	update(timestamp) {
+		const delta = timestamp - this.timestampLast;
+		this.timestampLast = timestamp;
+
+		// Skip first frame and frames where the player has tabbed out a while
+		if (this.timestampLast === null || delta > 1000) {
+			window.requestAnimationFrame(this.update.bind(this));
+			return;
+		}
+
+		if (this.entities !== null && this.terrain !== null) {
+			// Update entities
+			for (const entity of this.entities)
+				entity.update(delta, this.userinput);
+
+			// Draw background
+			const ctx = this.canvas.getContext('2d');
+			ctx.fillStyle = "black";
+			ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+			ctx.drawImage(this.terrain, 0, 0);
+
+			// Draw entities
+			for (const entity of this.entities)
+				ctx.drawImage(
+					entity.currentSprite,
+					this.cellsize * entity.x,
+					this.cellsize * entity.y
+				);
+		}
+
+		window.requestAnimationFrame(this.update.bind(this));
+		this.userinput = null;
 	}
 }
